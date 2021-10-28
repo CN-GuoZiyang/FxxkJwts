a63f2a031b8c39ffeecc6153a7d7c3f117f56c8a
Ziyang <guoziyang0033@gmail.com>
2021-10-29 01:06:46 +0800

    完成学年学期等选项

--- src/main/java/top/guoziyang/fxxkjwts/controllers/MainController.java
+++ src/main/java/top/guoziyang/fxxkjwts/controllers/MainController.java
@@ -9,16 +9,26 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import okhttp3.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import top.guoziyang.fxxkjwts.common.CourseUtil;
+import top.guoziyang.fxxkjwts.common.OkHttpUtil;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainController {
     private static Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    public static Map<String, String> xnxq = new HashMap<>();
+    public static Map<String, String> kkxq = new HashMap<>();
+    public static Map<String, String> kkyx = new HashMap<>();
 
     public static Stage stage;
 
@@ -47,6 +57,12 @@ public class MainController {
     // fuckPanel
     @FXML
     private ChoiceBox<String> categoriesChoice;
+    @FXML
+    private ChoiceBox<String> xnxqChoice;
+    @FXML
+    private ChoiceBox<String> kkxqChoice;
+    @FXML
+    private ChoiceBox<String> kkyxChoice;
 
     @FXML
     public void closeProgram() {
@@ -79,9 +95,68 @@ public class MainController {
         }
         categoriesChoice.setItems(FXCollections.observableArrayList(categoryName));
         categoriesChoice.setValue("--请选择--");
-        categoriesChoice.setOnAction(event -> {
-            //logger.info(categoriesChoice.getValue());
-        });
+        categoriesChoice.setOnAction(event -> getCourseBaseInfo());
+    }
+
+    private void getCourseBaseInfo() {
+        String value = categoriesChoice.getValue();
+        String courseCode = CourseUtil.courseCategoryMap.get(value);
+        Response response = OkHttpUtil.getWithCookie("http://jwts.hit.edu.cn/xsxk/queryXsxk?pageXklb=" + courseCode);
+        if(response == null || response.code() != 200) {
+            logger.error("network error");
+            return;
+        }
+        String content = null;
+        try {
+            content = response.body().string().replaceAll("\n", "");
+        } catch (IOException e) {
+            logger.error("network error");
+            e.printStackTrace();
+            return;
+        }
+
+        List<String> xnxqList = new ArrayList<>();
+        List<String> kkxqList = new ArrayList<>();
+        List<String> kkyxList = new ArrayList<>();
+        // 获取学年学期
+        xnxq.clear();
+        String pattern = "<option value=\"(.*?)\"[\\s]?>(.*?)</option>";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(content);
+        while(m.find()) {
+            if(m.group(2).contains("全部")) {
+                kkxq.put(m.group(2), m.group(1));
+                kkxqList.add(m.group(2));
+                break;
+            }
+            xnxq.put(m.group(2), m.group(1));
+            xnxqList.add(m.group(2));
+        }
+        xnxqChoice.setItems(FXCollections.observableArrayList(xnxqList));
+        xnxqChoice.setValue(xnxqList.get(0));
+
+        // 获取校区
+        kkxq.clear();
+        while (m.find()) {
+            if(m.group(2).contains("全部")) {
+                kkyx.put(m.group(2), m.group(1));
+                kkyxList.add(m.group(2));
+                break;
+            }
+            kkxq.put(m.group(2), m.group(1));
+            kkxqList.add(m.group(2));
+        }
+        kkxqChoice.setItems(FXCollections.observableArrayList(kkxqList));
+        kkxqChoice.setValue(kkxqList.get(0));
+
+        // 获取院系
+        kkyx.clear();
+        while (m.find()) {
+            kkyx.put(m.group(2), m.group(1));
+            kkyxList.add(m.group(2));
+        }
+        kkyxChoice.setItems(FXCollections.observableArrayList(kkyxList));
+        kkyxChoice.setValue(kkyxList.get(0));
     }
 
 }
